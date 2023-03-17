31faee192f4e2f32d36bb97e2041843ef6d730e7
Ratnesh Sharma <ratnesh@siliconithub.com>
2023-03-17 16:58:01 +0530

    Created App for AI Image Genrate.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
+
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        <img src={logo} alt="AI Image Gen." className="w-50 object-contain" />
+      </Link>
+
+      <Link to="/create-post" className="font-inter font-medium border border-[#000a64]  bg-[#000a64] hover:bg-white  text-white hover:text-[#000a64] text-[14px] lg:text-[16px] px-4 py-2 rounded-md">Create AI Image</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
